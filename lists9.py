84d3b767c5e2a92fe3c2c5414486e6838fa2eb8f
giorgio <playclevergames@mail.ru>
2015-10-02 16:05:09 +0300

    Последовательность флоатов

--- lists9.py
+++ lists9.py
@@ -1,7 +1,8 @@
 from random import *
 ins = open("float_data.txt","w")
 s = ""
-for i in range(1000000):
-    s += str(random()*100) + " "
+for i in range(0,1000000):
+    s += str(randint(0,10000)/100) + " "
+print(s)
 ins.write(s)
 ins.close()
